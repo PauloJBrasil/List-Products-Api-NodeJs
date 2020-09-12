74479c93556d4a4d9cd1c2bd68e0d2b466e938ac
PauloJBrasil <paulojosafa@hotmail.com>
2020-09-12 13:16:31 -0300

    Adicionado funcoes CRUD e funcionalidade CORS

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    
+const ProductController =  require('./controllers/ProductController');
 
-    return res.send('Hello World');
-});
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
