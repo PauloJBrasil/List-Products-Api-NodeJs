c3b0dcc2acb0a12c9f3aea796bbe47b330aba460
PauloJBrasil <paulojosafa@hotmail.com>
2020-09-12 12:38:24 -0300

    Adicionando Controller

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React Native',
-        url: 'https://github.com/facebook/react-native'
-    });
+    
 
     return res.send('Hello World');
 });
